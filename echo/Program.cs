9e09ee6df2175cd55e2e2464667eafc4be8d2a48
luowentao <luowentao@tal.com>
2022-08-29 18:20:39 +0800

    init:初始化网络基础demo

---
+++ echo/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Sockets;
+using System;
+using System.Net;
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello xx");
+        
+    }
+}
